bca68e7d7d4e5ca8e419b71be3bb264aab373046
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-22 17:51:12 +0200

    types on create

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Support\Str;
 
 
@@ -30,7 +31,9 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        $types = Type::all();
+        
+        return view('admin.projects.create', compact('types'));
     }
 
     /**
