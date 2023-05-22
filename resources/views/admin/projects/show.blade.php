1049c7ebd4ab76dddaab98756381d6bfce855882
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-05-22 19:03:47 +0200

    type->name

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <div class="container text-center p-3">
         <h1>{{ $project->title }}</h1>
+        @if ($project->type_id)
+            <h2>Type: {{ $project->type->name }}</h2>
+        @endif
         <p>{{ $project->description }}</p>
     </div>
 @endsection
